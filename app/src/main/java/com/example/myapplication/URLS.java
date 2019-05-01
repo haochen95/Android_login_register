575f767c81d33eee5f110656fb23fe59bbb5b628
haoch <haochen273@gmail.com>
2019-05-01 10:48:41 +0900

    first version

---
+++ app/src/main/java/com/example/myapplication/URLS.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+public class URLS {
+    private static final String ROOT_URL = "http://192.168.0.11/user/registrationapi.php?apicall=";
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
